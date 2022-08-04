3a6b96a27834b6c493257b0506309445cdad5314
Aleksandr Seledcov <98162012+AlexsandrSeledcov@users.noreply.github.com>
2022-08-04 09:06:13 +0300

    Revert "Dev"

--- Angular_winforms/ClientApp/src/app/app.module.ts
+++ Angular_winforms/ClientApp/src/app/app.module.ts
@@ -1,69 +1,34 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule, registerLocaleData } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { FillingComponent } from './filling/filling.component';
-import { ResultsComponent } from './results/results.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { CheckLicenseComponent } from './check-license/check-license.component';
-import { AboutTheProgramComponent } from './about-the-program/about-the-program.component';
-import { PipelineConfigurationComponent } from './pipeline-сonfiguration/pipeline-сonfiguration.component';
-import { VerticalPipelineParameters } from './vertical-pipeline-parameters.component/vertical-pipeline-parameters.component';
-
-import { OpenProjectComponent } from './open-project/open-project.component';
-import { OpenСonfigurationComponent } from './open-configuration/open-configuration.component';
-import { SaveProjectComponent } from './save-project/save-project.component';
-import { SaveСonfigurationComponent } from './save-configuration/save-configuration.component';
-
-import { DataService } from './_services/data.service';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    ResultsComponent,
-    FillingComponent,
-    AboutTheProgramComponent,
-    VerticalPipelineParameters,
-    PipelineConfigurationComponent,
-    CheckLicenseComponent,
-
-    OpenProjectComponent,
-    SaveProjectComponent,
-    OpenСonfigurationComponent,
-    SaveСonfigurationComponent
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    CommonModule,
-    FormsModule,
     HttpClientModule,
-
+    FormsModule,
     RouterModule.forRoot([
-      { path: '',                       component: VerticalPipelineParameters, pathMatch: 'full' },
-      { path: 'pipeline-сonfiguration', component: PipelineConfigurationComponent },
-      { path: 'about-the-program',      component: AboutTheProgramComponent },
-      { path: 'results',                component: ResultsComponent },
-      { path: 'filling',                component: FillingComponent },
-      { path: 'check-license',          component: CheckLicenseComponent },
-      { path: 'open-project',           component: OpenProjectComponent },
-      { path: 'save-project',           component: SaveProjectComponent },
-      { path: 'open-configuration',     component: OpenСonfigurationComponent },
-      { path: 'save-configuration',     component: SaveСonfigurationComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [DataService],
-
+  providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
